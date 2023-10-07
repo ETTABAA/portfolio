e72a1ce1529143f92f7bbf3b617a1479c4b1ffa9
ETTABAA Aomar <omar.ettabaa@gmail.com>
2023-10-07 23:52:16 +0100

    ajout d'un annimation au loago et au link quand la page s'actualise

--- assets/js/specialisation.js
+++ assets/js/specialisation.js
@@ -1,23 +1,39 @@
-const 
-    specialisation = document.querySelector('.specialisation'),
-    specialisationName = document.querySelector('.specialisationName');
 
-specialisation.addEventListener('mouseenter', (e) =>{
-    document.addEventListener('mousemove',moveText);
-})
+    const 
+        specialisation = document.querySelector('.specialisation'),
+        specialisationName = document.querySelector('.specialisationName h4'),
+        bigname = document.querySelector('.nomPrenom')
+        ;
 
-specialisation.addEventListener('mouseleave', (e) => {
-    document.removeEventListener('mousemove', moveText);
-    resetTextPosition();
-});
-function moveText(e) {
-    const rect = specialisation.getBoundingClientRect();
-    const x = e.clientX - rect.left;
-    const y = e.clientY - rect.top;
-    specialisationName.style.transform = `translate(${x}px, ${y}px)`;
-}
-function resetTextPosition() {
-    specialisationName.style.display = 'flex';
-    specialisationName.style.justifyContent = 'center';
+        const insertSpecialisation = (text) => {
+            const chars = text.split('');
+            const elements = chars.map((char,index) => `<span style="--i : " ${index+1} >${char}</span>`);
+            return elements.join('');
+        };
+        const lettresSpecialisaion = document.querySelectorAll('.specialisationName h4 span');
+        specialisationName.innerHTML += `<div>${insertSpecialisation('Front-End')}</div>`;
+        specialisationName.innerHTML += `<div>${insertSpecialisation('Developer')}</div>`;
+        bigname.innerHTML += `<span class='nom'>${insertSpecialisation('ETTABAA')}</span>`;
+        bigname.innerHTML += `<span class='nom'>${insertSpecialisation('Aomar')}</span>`;
+        const bignamediv = document.querySelectorAll('.nomPrenom div');
+        bignamediv[0].classList.add('nom');
+        bignamediv[1].classList.add('prenom');
 
-}
+    //     const nom = document.querySelectorAll('.nom span');
+    //     const prenom = document.querySelectorAll('.prenom span');
+    //     const addlettreenboucle = (table)=>{
+    //         let delai =0;
+    //         table.forEach(element => {
+    //             setTimeout(()=> element.style.display ='inline'
+    //             ,delai);
+    //             delai += 500;
+    //         })
+    //     }
+    //     const nomDiv = document.querySelector('.nom');
+    //     setInterval(() => {
+    //         addlettreenboucle(nom);
+    //       }, 1000);
+        
+
+
+
